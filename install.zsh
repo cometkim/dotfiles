2948d5c1c54635eb95df354de5cfd795f33fd1e2
Hyeseong Kim <hey@hyeseong.kim>
2022-12-11 11:11:01 +0900

    backup yarn v1 mirror setting

--- install.zsh
+++ install.zsh
@@ -88,6 +88,7 @@ link "gitconfig" "$HOME/.gitconfig"
 link "tool-versions" "$HOME/.tool-versions"
 link "tmux/.tmux.conf" "$HOME/.tmux.conf"
 link "tmux/.tmux.conf.local" "$HOME/.tmux.conf.local"
+link "yarnrc" "$HOME/.yarnrc"
 
 link "config/nvim" "$CONFIG/nvim"
 link "config/kime" "$CONFIG/kime"
