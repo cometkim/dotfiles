859bfbadf3f24256a9523e3e209d5843f4edae1c
Hyeseong Kim <hey@hyeseong.kim>
2025-04-26 07:02:13 +0900

    update config

--- config/nvim/lua/plugins/neominimap.lua
+++ config/nvim/lua/plugins/neominimap.lua
@@ -1,4 +1,3 @@
----@module "neominimap.config.meta"
 return {
   "Isrothy/neominimap.nvim",
   version = "v3.12.0",
@@ -6,14 +5,13 @@ return {
   keys = {
     -- Global Minimap Controls
     { "<leader>mm", "<cmd>Neominimap toggle<cr>", desc = "Toggle global minimap" },
-    -- { "<leader>nr", "<cmd>Neominimap refresh<cr>", desc = "Refresh global minimap" },
+    { "<leader>nr", "<cmd>Neominimap refresh<cr>", desc = "Refresh global minimap" },
   },
   init = function()
     -- The following options are recommended when layout == "float"
     -- vim.opt.wrap = false
     -- vim.opt.sidescrolloff = 36 -- Set a large value
 
-    ---@type Neominimap.UserConfig
     vim.g.neominimap = {
       auto_enable = true,
     }
