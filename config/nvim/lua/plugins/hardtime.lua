851bf24014d24f37c37be49ab4018477f441e66f
Hyeseong Kim <hey@hyeseong.kim>
2025-05-22 15:23:58 +0900

    tooo hardtime...

--- config/nvim/lua/plugins/hardtime.lua
+++ config/nvim/lua/plugins/hardtime.lua
@@ -2,5 +2,9 @@ return {
   "m4xshen/hardtime.nvim",
   lazy = false,
   dependencies = { "MunifTanjim/nui.nvim" },
-  opts = {},
+  opts = {
+    max_time = 3000,
+    max_count = 10,
+    disable_mouse = false,
+  },
 }
