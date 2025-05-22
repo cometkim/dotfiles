0048e6c6ba334bb0f33556c988527af3133d0907
Hyeseong Kim <hey@hyeseong.kim>
2025-05-22 09:20:04 +0900

    cmdline and overseer

---
+++ config/nvim/lua/plugins/overseer.lua
@@ -0,0 +1,5 @@
+return {
+  "stevearc/overseer.nvim",
+  event = "VeryLazy",
+  opts = {},
+}
--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,25 @@
 -- Already configured by NvChad
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'jonarrien/telescope-cmdline.nvim',
+  },
+  keys = {
+    { "<leader><leader>", "<cmd>Telescope cmdline<cr>", desc = "Cmdline" },
+  },
+  opts = {
+    extensions = {
+      cmdline = {
+        overseer = {
+          enabled = true,
+        },
+      },
+    },
+  },
   config = function(_, opts)
     dofile(vim.g.base46_cache .. "telescope")
     require("telescope").setup(opts)
+    require("telescope").load_extension("cmdline")
   end
 }
