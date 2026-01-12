278c1564ae70c2ac77e11c2dcd7f70d746bd11fa
Hyeseong Kim <hey@hyeseong.kim>
2026-01-12 13:07:12 +0900

    autoread buffers

---
+++ config/nvim/lua/autocmds.lua
@@ -0,0 +1,6 @@
+require "nvchad.autocmds"
+
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = "*",
+})
--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -2,6 +2,7 @@ require "nvchad.options"
 
 local o = vim.o
 o.cursorlineopt = "both" -- to enable cursorline!
+o.autoread = true
 
 local ft = vim.filetype
 ft.add {
