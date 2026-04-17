3e616c3a3cc2286d59d159003b576443408a41a2
Hyeseong Kim <hey@hyeseong.kim>
2026-04-17 18:17:02 +0900

    update nvim-treesitter

--- config/nvim/lua/plugins/neominimap.lua
+++ config/nvim/lua/plugins/neominimap.lua
@@ -1,11 +1,11 @@
 return {
   "Isrothy/neominimap.nvim",
-  version = "v3.12.0",
+  version = "v3.16.0",
   lazy = false,
   keys = {
     -- Global Minimap Controls
-    { "<leader>mm", "<cmd>Neominimap toggle<cr>",  desc = "Toggle global minimap" },
-    { "<leader>mr", "<cmd>Neominimap refresh<cr>", desc = "Refresh global minimap" },
+    { "<leader>mm", "<cmd>Neominimap Toggle<cr>",  desc = "Toggle global minimap" },
+    { "<leader>mr", "<cmd>Neominimap Refresh<cr>", desc = "Refresh global minimap" },
   },
   init = function()
     -- The following options are recommended when layout == "float"
@@ -27,9 +27,9 @@ return {
         local buf_lines = vim.api.nvim_buf_line_count(0)
         local win_height = vim.api.nvim_win_get_height(0)
         if buf_lines > win_height then
-          vim.cmd("Neominimap on")
+          vim.cmd("Neominimap Enable")
         else
-          vim.cmd("Neominimap off")
+          vim.cmd("Neominimap Disable")
         end
       end,
     })
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,74 +1,94 @@
+dofile(vim.g.base46_cache .. "treesitter")
+
+local ensure_installed = {
+  "astro",
+  "bash",
+  "capnp",
+  "css",
+  "csv",
+  "diff",
+  "dockerfile",
+  "ebnf",
+  "editorconfig",
+  "git_config",
+  "git_rebase",
+  "gitattributes",
+  "gitcommit",
+  "gitignore",
+  "glsl",
+  "go",
+  "gomod",
+  "gosum",
+  "gotmpl",
+  "graphql",
+  "hcl",
+  "html",
+  "hurl",
+  "ini",
+  "javascript",
+  "jsdoc",
+  "json",
+  "json5",
+  "kdl",
+  "kotlin",
+  "lua",
+  "menhir",
+  "nginx",
+  "ocaml",
+  "ocaml_interface",
+  "ocamllex",
+  "pem",
+  "prisma",
+  "proto",
+  "python",
+  "query",     -- Tree-Sitter query language
+  "rescript",
+  "robots_txt",
+  "ron",
+  "ruby",
+  "rust",
+  "sql",
+  "starlark",
+  "svelte",
+  "swift",
+  "textproto",
+  "thrift",
+  "toml",
+  "typescript",
+  "vim",
+  "vimdoc",
+  "vue",
+  "wgsl",
+  "wit",
+  "xml",
+  "yaml",
+  "zig",
+}
+
 return {
   "nvim-treesitter/nvim-treesitter",
-  config = function(_, opts)
-    dofile(vim.g.base46_cache .. "treesitter")
-    require("nvim-treesitter.configs").setup(opts)
+  branch = "main",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    local ts = require "nvim-treesitter"
+
+    local already_installed = ts.get_installed()
+    local parsers_to_install = vim.iter(ensure_installed)
+    :filter(function(parser)
+      return not vim.tbl_contains(already_installed, parser)
+    end)
+    :totable()
+
+    ts.install(parsers_to_install)
+
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function()
+        -- Enable treesitter highlighting and disable regex syntax
+        pcall(vim.treesitter.start)
+        -- Enable treesitter-based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
   end,
-  opts = {
-    ensure_installed = {
-      "astro",
-      "bash",
-      "capnp",
-      "css",
-      "csv",
-      "diff",
-      "dockerfile",
-      "ebnf",
-      "editorconfig",
-      "git_config",
-      "git_rebase",
-      "gitattributes",
-      "gitcommit",
-      "gitignore",
-      "glsl",
-      "go",
-      "gomod",
-      "gosum",
-      "gotmpl",
-      "graphql",
-      "hcl",
-      "html",
-      "hurl",
-      "ini",
-      "javascript",
-      "jsdoc",
-      "json",
-      "json5",
-      "jsonc",
-      "kdl",
-      "kotlin",
-      "lua",
-      "menhir",
-      "nginx",
-      "ocaml",
-      "ocaml_interface",
-      "ocamllex",
-      "pem",
-      "prisma",
-      "proto",
-      "python",
-      "query", -- Tree-Sitter query language
-      "rescript",
-      "robots",
-      "ron",
-      "ruby",
-      "rust",
-      "sql",
-      "starlark",
-      "svelte",
-      "swift",
-      "textproto",
-      "thrift",
-      "toml",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "vue",
-      "wgsl",
-      "wit",
-      "xml",
-      "yaml",
-      "zig",
-    },
-  },
 }
